ce32152ba3c3722c971a68cb4f579c0029a4f208
martinloretzzz <20306567+martinloretzzz@users.noreply.github.com>
2025-02-07 01:03:11 +0100

    Read typewriter to serial

--- typewriter/typewriter.ino
+++ typewriter/typewriter.ino
@@ -1,55 +1,85 @@
 #define ROW_SELECT_PIN 4
 
-long startOfCycle = 0;
+String keyMapping[8][9] = {
+  { "5", "8", "1", "9", "ß", "FILE", "TW/WP", "`", "4" },
+  { "6", "7", "2", "j", "0", "ö", "CANCEL", "l", "3" },
+  { "r", "z", "q", "i", "p", "+", "ENTER", "#", "w" },
+  { "t", "h", "y", "u", "o", "ü", "IND REV", "k", "e" },
+  { "c", "g", "a", "b", "m", "LEFT", "DEL", ".", "d" },
+  { "f", "v", "s", "n", ",", "DOWN", "WORD OUT", "-", "x" },
+  { "", "", "", "", "", "CODE", "SPACE", "ä", "" },
+  { "UP", "", "LCAPS", "RIGHT", "MENU", "", "SHIFT", "", "P IND" }
+};
 
-void rowSelectISR()
-{
+int pinMapping[8] = { 23, 22, 21, 19, 18, 5, 17, 16 };
+
+long startOfCycle = 0;
+String cycleKey = "";
+bool newCycle = false;
+
+String lastKey = "";
+long startOfCurrentKey = 0;
+String currentKey = "";
+
+void rowSelectISR() {
   startOfCycle = micros();
+  newCycle = true;
 }
 
-void setup()
-{
+void setup() {
   Serial.begin(115200);
-  pinMode(23, INPUT);
-  pinMode(22, INPUT);
-  pinMode(21, INPUT);
-  pinMode(19, INPUT);
-  pinMode(18, INPUT);
-  pinMode(5, INPUT);
-  pinMode(17, INPUT);
-  pinMode(16, INPUT);
+  for (int p = 0; p < 8; p++) {
+    pinMode(pinMapping[p], INPUT);
+  }
   pinMode(ROW_SELECT_PIN, INPUT);
 
   attachInterrupt(ROW_SELECT_PIN, rowSelectISR, RISING);
 }
 
-void printPinActive(int pin)
-{
-  long cycleOffset = ((micros() - (startOfCycle - 2000)) % 18000);
-
-  for (int i = 0; i < 9; i++)
-  {
-    if (digitalRead(pin) == LOW && 2000 * i + 500 < cycleOffset && cycleOffset < 2000 * (i + 1) - 500)
-    {
-      Serial.print(pin);
-      Serial.print(" ");
-      Serial.print(i);
-      Serial.print(" ");
-      Serial.print(cycleOffset);
-      Serial.println("");
+String getKey() {
+  long cycleOffset = (micros() - startOfCycle) - 2000;
+  for (int t = -1; t < 8; t++) {
+    if (2000 * t + 500 < cycleOffset && cycleOffset < 2000 * (t + 1) - 500) {
+      for (int p = 0; p < 8; p++) {
+        if (digitalRead(pinMapping[p]) == LOW) {
+          return keyMapping[p][t + 1];
+        }
+      }
     }
+  }
+  return "";
+}
+
+void onKey(String key) {
+  Serial.println(key);
+}
+
+void onCycleKey(String key) {
+  if (key != currentKey && key != lastKey) {
+    currentKey = key;
+    startOfCurrentKey = millis();
+  }
+
+  if (key == currentKey && millis() - startOfCurrentKey > 20) {
+    if (key != "") {
+      onKey(key);
+    }
+    lastKey = key;
+    currentKey = "";
   }
 }
 
-void loop()
-{
-  printPinActive(23);
-  printPinActive(22);
-  printPinActive(21);
-  printPinActive(19);
-  printPinActive(18);
-  printPinActive(5);
-  printPinActive(17);
-  printPinActive(16);
+void loop() {
+  String pressedKey = getKey();
+  if (pressedKey != "") {
+    cycleKey = pressedKey;
+  }
+
+  if (newCycle) {
+    onCycleKey(cycleKey);
+    newCycle = false;
+    cycleKey = "";
+  }
+
   delayMicroseconds(500);
 }
