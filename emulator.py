2d04cbffea61ab4ca831e6581f39400933beca2f
martinloretzzz <20306567+martinloretzzz@users.noreply.github.com>
2025-02-26 21:19:19 +0100

    Stream output in emulator

--- emulator.py
+++ emulator.py
@@ -1,4 +1,13 @@
 import requests
+import time
+import sys
+
+def print_stram(text, delay=0.04):
+    text = text.encode().decode('unicode_escape')
+    for char in text:
+        sys.stdout.write(char)
+        sys.stdout.flush()
+        time.sleep(delay)  
 
 if __name__ == "__main__":
     while True:
@@ -6,4 +15,4 @@ if __name__ == "__main__":
         r = requests.get(f"http://127.0.0.1:8000/command/{command}")
         answer = str(r.text)
         answer = answer[1:-1] if answer.startswith('"') and answer.endswith('"') else answer
-        print(f"{answer} ")
+        print_stram(f"{answer} ")
